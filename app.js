6fbcc5259b1021779637fc85c91405993fee7f5e
Seth Deegan <jayandseth@gmail.com>
2018-03-19 20:23:36 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var express = require('express');
+var app = express();
+const { Client } = require('pg');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Dnew') {
+    msg.reply('testing:working');
+  }
+});
+
+/*
+await client.connect()
+
+const res = await client.query('SELECT $1::text as message', ['Hello world!']);
+console.log(res.rows[0].message); // Hello world!
+await client.end();
+
+const messages = msg => {
+  if (msg.content === 'help') {
+    msg.reply('pong');
+  }
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', messages);
+
+*/
+
+client.login(process.env.token);
